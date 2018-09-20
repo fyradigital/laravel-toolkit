1c7cb12dbf1f40c0a15becc70db98258cadb60fd
Edward Sides <esides@fyra.io>
2018-09-19 21:02:11 -0400

    change trait name

--- src/LaravelToolkit.php
+++ src/LaravelToolkit.php
@@ -3,12 +3,12 @@
 namespace FyraDigital\LaravelToolkit;
 
 use FyraDigital\LaravelToolkit\Traits\Helpers;
-use FyraDigital\LaravelToolkit\Traits\String;
+use FyraDigital\LaravelToolkit\Traits\Strings;
 
 class Fyra
 {
     use Helpers;
-    use String;
+    use Strings;
 
     public $version = '1.0.0';
 }
