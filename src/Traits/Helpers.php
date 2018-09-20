32536fcb9b59b31b7aa540475790a3d52ce3084b
Edward Sides <esides@fyra.io>
2018-09-19 21:22:23 -0400

    refactoring

--- src/Traits/Helpers.php
+++ src/Traits/Helpers.php
@@ -4,40 +4,36 @@ namespace FyraDigital\LaravelToolkit\Traits;
 
 trait Helpers
 {
-    public function loadAsset($name, $versioning='true') {
+    public function getVersion($versioning){
         $versioning = strtolower($versioning);
+        if ('never'==$versioning) {
+            return '';
+        } elseif ('local'==env('APP_ENV')) {
+            return '?v=' . time();
+        } elseif ('true'===$versioning ||) {
+            return '?v=' . env('APP_BUILD');
+        }
+    }
+    public function loadAsset($name, $versioning='default') {
+        if ('default'==$versioning && strpos(strtolower($name), 'cloudfront.net')>0) {
+            $version = '';
+        } else {
+            $version = getVersion($versioning);
+        }
         switch (strtolower(substr($name, -3))) {
             case '.js':
                 $strHTML = '<script src="';
-                $strHTML .= url($name);
-                if ('never'==$versioning) {
-                } elseif ('local'==env('APP_ENV')) {
-                    $strHTML .= '?v=' . time();
-                } elseif ('true'===$versioning) {
-                    $strHTML .= '?v=' . env('APP_BUILD');
-                }
+                $strHTML .= url($name) . $version;
                 $strHTML .= '" type="text/javascript"></script>';
                 break;
             case 'css':
                 $strHTML = '<link href="';
-                $strHTML .= url($name);
-                if ('never'==$versioning) {
-                } elseif ('local'==env('APP_ENV')) {
-                    $strHTML .= '?v=' . time();
-                } elseif ('true'===$versioning) {
-                    $strHTML .= '?v=' . env('APP_BUILD');
-                }
+                $strHTML .= url($name) . $version;
                 $strHTML .= '" rel="stylesheet" type="text/css" />';
                 break;
             default:
                 $strHTML = '<img src="';
-                $strHTML .= url($name);
-                if ('never'==$versioning) {
-                } elseif ('local'==env('APP_ENV')) {
-                    $strHTML .= '?v=' . time();
-                } elseif ('true'===$versioning) {
-                    $strHTML .= '?v=' . env('APP_BUILD');
-                }
+                $strHTML .= url($name) . $version;
                 $strHTML .= '"/>';
                 break;
         }
